a5968ea96375bcb2e5f36e6c444f3626144bdf24
Coder <yadavsuhail8@gmail.com>
2024-12-29 13:45:08 +0530

    created login, profile and logout routes for captain

--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { body } = require('express-validator')
 const captainController = require('../controllers/captain.controllers')
+const authMiddleware = require('../middlewares/auth.middlewares')
 
 router.post('/register', [
   body('email').isEmail().withMessage('Invalid Email'),
@@ -14,6 +15,17 @@ router.post('/register', [
 ],
   captainController.registerCaptain
 )
+
+router.post('/login', [
+  body('email').isEmail().withMessage('Invalid Email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 character long')
+],
+  captainController.loginCaptain
+)
+
+router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile)
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain)
 
 
 
