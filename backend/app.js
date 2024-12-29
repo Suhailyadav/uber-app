097805d97d385cb68129bb7e4f911b102b301ea5
Coder <yadavsuhail8@gmail.com>
2024-12-29 12:22:43 +0530

    created login, logout and profile route for user and login route for captain

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,24 @@ const dotenv = require('dotenv')
 dotenv.config();
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const connectToDb = require('./db/db')
+connectToDb();
+const userRoutes = require('./routes/user.routes')
+const captainRoutes = require('./routes/captain.routes')
+
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('hello')
 })
+
+app.use('/users', userRoutes)
+app.use('/captains', captainRoutes)
 
 module.exports = app
