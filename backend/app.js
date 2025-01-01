5d798f635acebace5083e8f177de498137e7a579
Coder <yadavsuhail8@gmail.com>
2025-01-01 23:39:33 +0530

    uber-app-backend

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const connectToDb = require('./db/db')
 connectToDb();
 const userRoutes = require('./routes/user.routes')
 const captainRoutes = require('./routes/captain.routes')
+const mapsRoutes = require('./routes/maps.routes')
+const rideRoutes = require('./routes/ride.routes')
 
 const app = express()
 
@@ -21,5 +23,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes)
 app.use('/captains', captainRoutes)
+app.use('/maps', mapsRoutes)
+app.use('/rides', rideRoutes)
 
 module.exports = app
